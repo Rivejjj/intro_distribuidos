7460b87efd623d47a2fa6bb86c812b478b3d0ec5
Higlak <jaschieri@fi.uba.ar>
2023-11-21 02:42:31 -0300

    listo, emprolijar cositas falta capas

--- firewall.py
+++ firewall.py
@@ -3,178 +3,113 @@ import pox.openflow.libopenflow_01 as of
 from pox.lib.revent import *
 from pox.lib.util import dpidToStr
 from pox.lib.addresses import IPAddr
-from collections import namedtuple
+#from collections import namedtuple
 import json
 import os
 
-
+IPV4 = 0x800
+IPV6 = 0x86dd
 TCP = 6
 UDP = 17
-# IP_PROTO_ICMP: 1
-# IP_PROTO_IGMP: 2
-# IP_PROTO_GRE: 47
-# IP_PROTO_OSPF: 89
-# IP_PROTO_PIM: 103
-# IP_PROTO_VRRP: 112
-# IP_PROTO_SCTP: 132
-
 
 log = core.getLogger()
-policyFile = "%s/pox/pox/misc/firewall-policies.csv" % os.environ[ 'HOME' ]
 
-''' Add your global variables here ... '''
+rules = []
+firewall_dpid = None
 
-hosts = {}
+class FirewallRule():
+    def __init__(self, ip_protocol, src_ip, dst_ip, transport_protocol, src_port, dst_port):
+        self.dst_ip = dst_ip
+        self.src_ip = src_ip
+        if src_ip != None:
+            self.src_ip = IPAddr(src_ip)
+        if dst_ip != None:
+            self.dst_ip = IPAddr(dst_ip)
 
+        self.src_port = src_port
+        self.dst_port = dst_port
+        if ip_protocol == "IPV4" or ip_protocol == IPV4:
+            self.ip_protocol = IPV4
+        elif ip_protocol == "IPV6" or ip_protocol == IPV6:
+            self.ip_protocol = IPV6
+        else:
+            self.ip_protocol = None
+        if transport_protocol == "UDP" or transport_protocol == UDP:
+            self.transport_protocol = UDP
+        elif transport_protocol == "TCP" or transport_protocol == TCP:
+            self.transport_protocol = TCP
+        else:
+            self.transport_protocol = None
+    
+    def __str__(self):
+        return f"ip_protocol = {self.ip_protocol} src_ip = {self.src_ip},dst_ip = {self.dst_ip}, src_port = {self.src_port}, dst_port = {self.dst_port}, transport_protocol = {self.transport_protocol} \n"
+    
+    def add_to_rules(self):
+        if (self.src_port != None or self.dst_port != None) and self.transport_protocol == None:
+            rule1 = FirewallRule(self.ip_protocol, self.src_ip, self.dst_ip, "UDP", self.src_port, self.dst_port)
+            rule2 = FirewallRule(self.ip_protocol, self.src_ip, self.dst_ip, "TCP", self.src_port, self.dst_port)
+            rule1.add_to_rules()
+            rule2.add_to_rules()
+        elif (self.src_ip != None or self.dst_ip != None or self.transport_protocol != None) and self.ip_protocol == None:
+            rule1 = FirewallRule("IPV4",self.src_ip, self.dst_ip, self.transport_protocol, self.src_port, self.dst_port)
+            rule2 = FirewallRule("IPV6",self.src_ip, self.dst_ip, self.transport_protocol, self.src_port, self.dst_port)
+            rule1.add_to_rules()
+            rule2.add_to_rules()
+        else:
+            rules.append(self)
+    
 
 class Firewall (EventMixin):
-
-    def _load_rule_from_file(self, file_path):
-        """
-        Loads a firewall rule from a JSON file.
-
-        Parameters:
-        - file_path (str): The path to the JSON file containing the firewall rule.
-
-        Raises:
-        - FileNotFoundError: If the specified file_path does not exist.
-        - json.JSONDecodeError: If there is an issue decoding the JSON content.
-
-        Returns:
-        - dict: The loaded firewall rule as a dictionary.
-        """
+    
+    def _load_rules_from_file(self, file_path):
+        global firewall_dpid 
         try:
             with open(file_path, 'r') as file:
-                self.rules_data = json.load(file)
-                log.debug(f"Loaded rules from {file_path}: {self.rules_data}")
-            return self.rules_data
+                read_rules = json.load(file)
+                for rule in read_rules.values():
+                    if firewall_dpid == None:
+                        firewall_dpid = rule
+                    else:
+                        created_rule = FirewallRule(
+                        rule.get("ip_protocol"),
+                        rule.get("src_host"),
+                        rule.get("dst_host"),
+                        rule.get("transport_protocol"),
+                        rule.get("src_port"),
+                        rule.get("dst_port"))
+                        created_rule.add_to_rules()
+                    
         except FileNotFoundError:
-            log.error(f"File not found: {file_path}")
+            log.error(f"Error processing file: {file_path}")
             raise
-        except json.JSONDecodeError:
-            log.error(f"Error decoding JSON in file: {file_path}")
-            raise
-
 
     def __init__(self, rules_file_path):
-        """
-        Initializes the Firewall module.
-
-        Parameters:
-        - rules_file_path (str): The file path to the JSON file containing firewall rules.
-
-        This method listens to OpenFlow events, loads firewall rules from the specified file,
-        and enables the Firewall Module.
-
-        Args:
-        - rules_file_path (str): The file path to the JSON file containing firewall rules.
-        """
         self.listenTo(core.openflow)
-        self._load_rule_from_file(rules_file_path)
+        self._load_rules_from_file(rules_file_path)
+        log.debug(f"{rules}")
+        for rule in rules:
+            log.debug(f"{rule}")
         log.debug("Enabling Firewall Module")
-
+        
 
     def _handle_ConnectionUp (self, event):
-        ''' Add your logic here ... '''
-        log.debug("Firewall rules installed on %s", dpidToStr(event.dpid))
+        if int(event.dpid) == firewall_dpid:
+            log.debug("Firewall rules installed on %s", dpidToStr(event.dpid))
+            for rule in rules:
+                self.add_firewall_rule(event.connection, rule)
         
-        # Rule 1
-        self.apply_rule_1(event)
-        
-        # # Rule 2
-        self.apply_rule_2(event)
-
-        # Rule 3
-        self.apply_rule_3(event)
-
-
-    def apply_rule_1(self, event):
-        """
-        Apply the first rule: Discard all messages with destination port 80.
-
-        Parameters:
-        - event: OpenFlow event triggered when a switch connection is established.
-
-        This function uses the add_firewall_rule function to add a rule to the switch
-        that discards all messages with destination port 80.
-        """
-        rule_1 = self.rules_data.get("rule_1")
-        dst_port = rule_1.get("dst_port")
-        log.debug(f"Applying Rule 1: Discard messages with destination port {dst_port}")
-        # Pregunta: Porque solo filtramos solo conexiones TCP?
-        self.add_firewall_rule(event.connection, transport_protocol=TCP,dst_port=dst_port)
-        log.debug("Rule 1 applied successfully.")
-
-
-    def apply_rule_2(self, event):
-        """
-        Apply the second rule: Discard messages from host 1 with destination port 5001 and using UDP.
-
-        Parameters:
-        - event: OpenFlow event triggered when a switch connection is established.
-
-        This function uses the add_firewall_rule function to add a rule to the switch
-        that discards all messages from host 1 with destination port 5001 and using UDP.
-        """
-        rule_2 = self.rules_data.get("rule_2")
-        str_ip = rule_2.get("src_host")
-        h1 = IPAddr(str_ip)
-        protocol = rule_2.get("protocol")
-        dst_port = rule_2.get("dst_port")
-        log.debug(f"Applying Rule 2: Discard messages from host {h1} with destination port {dst_port} and using UDP.")
-        self.add_firewall_rule(event.connection, dst_ip=h1,transport_protocol=protocol,dst_port=dst_port)
-        log.debug("Rule 2 applied successfully.")
-
-    def apply_rule_3(self, event):
-        """
-        Apply the third rule: Prevent communication between two specified hosts.
-
-        Parameters:
-        - event: OpenFlow event triggered when a switch connection is established.
-
-        This function uses the add_firewall_rule function to add rules to the switch
-        that prevent communication between the specified hosts.
-        """
-        log.debug("Applying Rule 3: Prevent communication between two specified hosts.")
-        rule_3 = self.rules_data.get("rule_3")
-        str_ip = rule_3.get("src_host")
-        h1 = IPAddr(str_ip)
-        str_ip = rule_3.get("dst_host")
-        h2 = IPAddr(str_ip)
-        log.debug(f"Applying Rule 3: Prevent communication between {h1} and {h2}.")
-        self.add_firewall_rule(event.connection, h1, h2)
-        self.add_firewall_rule(event.connection, h2, h1)
-        log.debug("Rule 3 applied successfully.")
-
-    def add_firewall_rule(self, connection, src_ip=None, dst_ip=None, transport_protocol=None, src_port=None, dst_port=None):
-        """
-        Add a rule to the switch using an OpenFlow message to filter packets.
-
-        Parameters:
-        - connection: OpenFlow connection object to the switch.
-        - src_ip: Source IP address for the rule (optional).
-        - dst_ip: Destination IP address for the rule (optional).
-        - transport_protocol: Transport protocol for the rule (optional).
-        - src_port: Source port for the rule (optional).
-        - dst_port: Destination port for the rule (optional).
-
-        Note: Unspecified parameters are set to None.
-
-        The function creates an OpenFlow message with the specified rule and sends it to the switch.
-        """
+    def add_firewall_rule(self, connection, rule):
         msg = of.ofp_flow_mod()
-        msg.match.dl_type = 0x800
-        msg.match.nw_src = src_ip
-        msg.match.nw_dst = dst_ip
-        msg.match.nw_proto = transport_protocol
-        msg.match.tp_src = src_port
-        msg.match.tp_dst = dst_port
-        msg.hard_timeout = 0  # Permanente
+        msg.match.dl_type = rule.ip_protocol
+        msg.match.nw_src = rule.src_ip
+        msg.match.nw_dst = rule. dst_ip
+        msg.match.nw_proto = rule.transport_protocol
+        msg.match.tp_src = rule.src_port
+        msg.match.tp_dst = rule.dst_port
+        msg.hard_timeout = 0
         connection.send(msg)
 
 
 def launch (rule="rule.json"):
-    '''
-    Starting the Firewall module
-    '''
+    
     core.registerNew(Firewall, rule)
--- topology.py
+++ topology.py
@@ -2,13 +2,6 @@ from mininet.topo import Topo
 
 class CustomTopology(Topo):
     def __init__(self, num_switches=4):
-        """
-        Constructor de la topología personalizada.
-
-        Parameters:
-        - num_switches: Número de switches en la parte dinámica (por defecto, 4).
-        """
-        # Llamada al constructor de la clase base (Topo)
         if num_switches <= 0:
             raise ValueError("The number of switches must be a non-negative value.")
     
@@ -17,7 +10,6 @@ class CustomTopology(Topo):
         # Parte estática - Izquierda
         h1 = self.addHost('h1')
         h2 = self.addHost('h2')
-        
 
         # Parte estática - Derecha
         h3 = self.addHost('h3')
@@ -36,8 +28,6 @@ class CustomTopology(Topo):
 
         self.addLink(switches[-1], h3)
         self.addLink(switches[-1], h4)
-        
-
         
 # globa topos
 topos = { 'myTopo': CustomTopology }
